821b89efccab27937792958c185199dab6f49e66
Cedric Decoster <cedric.decoster@gmail.com>
2021-07-13 23:10:32 +0200

    Projektdateien hinzufügen.

---
+++ PalletTemplateExt/ExtensionCall.cs
@@ -0,0 +1,14 @@
+﻿using SubstrateNetApi.Model.Types.Base;
+using SubstrateNetApi.Model.Types.Custom;
+using System;
+
+namespace SubstrateNetApi.Model.Calls
+{
+    public class ExtensionCall
+    {
+        public static GenericExtrinsicCall DoSomething(U32 something)
+        {
+            return new GenericExtrinsicCall("ConnectFour", "do_something", something);
+        }
+    }
+}
